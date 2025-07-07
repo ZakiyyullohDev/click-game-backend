e39e06e747c2339567ac66ead09a3e47f57bd2cd
ZakiyyullohDev <zakiyyullohm2010@gmail.com>
2025-07-07 10:05:44 +0500

    fix pg.js

--- src/lib/pg.js
+++ src/lib/pg.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const pg = require('pg');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new pg.Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    
+    ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 async function uniqRow(query, ...arr) {
